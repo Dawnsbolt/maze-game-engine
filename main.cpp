7a49f79e788fc39f43dd13a5e0ffe74f6e3a2985
Dawnsbolt <cryonicfuse@gmail.com>
2024-08-02 14:23:35 -0700

    initialized state

--- main.cpp
+++ main.cpp
@@ -1,27 +1,41 @@
 #include "main.h"
-Array2d<int,ROW,COL> map;
+Array2d<char,ROW,COL> map;
+Array2d<char,ROW,COL> memory;
+int pos_x, pos_y;
 
 int main() {
+    initialize_character(1,2);
     load_map();
-    draw_map();
+    update_memory();
+    draw_memory();
     return 0;
 }
 
 void load_map() {
     map = {{
-                            {1,1,1,1,1},
-                            {1,0,0,0,1},
-                            {1,0,1,0,1},
-                            {1,0,0,0,1},
-                            {1,1,1,1,1}
+                            {'1','1','1','1','1'},
+                            {'1','0','0','0','1'},
+                            {'1','0','1','0','1'},
+                            {'1','0','0','0','1'},
+                            {'1','1','1','1','1'}
     }};
 }
 
-void draw_map() {
+void draw_memory() {
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
-            std::cout << map[i][j];
+            std::cout << memory[i][j];
         }
         std::cout << '\n';
     }
+}
+
+void update_memory() {
+    memory = map;
+    memory[pos_x][pos_y] = 'C';
+}
+
+void initialize_character(int x, int y) {
+    pos_x = x;
+    pos_y = y;
 }
--- main.h
+++ main.h
@@ -7,5 +7,7 @@ const int COL = 5;
 template <typename T, std::size_t Row, std::size_t Col>
 using Array2d = std::array<std::array<T, Col>, Row>;
 
-void draw_map();
+void draw_memory();
 void load_map();
+void update_memory();
+void initialize_character(int,int);
