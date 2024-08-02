0051cda9f4a8998709af4c28839775cc39eab04f
Dawnsbolt <cryonicfuse@gmail.com>
2024-08-02 14:05:34 -0700

    basic draw/map

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include "main.h"
-Array2d<int,ROW,COL> map    {{
-                            {1,1,1,1,1},
-                            {1,0,0,0,1},
-                            {1,0,0,0,1},
-                            {1,0,0,0,1},
-                            {1,1,1,1,1}
-                        }};
+Array2d<int,ROW,COL> map;
+
 int main() {
-    
+    load_map();
+    draw_map();
     return 0;
 }
 
-void draw_map(Array2d<int,ROW,COL> *map);
+void load_map() {
+    map = {{
+                            {1,1,1,1,1},
+                            {1,0,0,0,1},
+                            {1,0,1,0,1},
+                            {1,0,0,0,1},
+                            {1,1,1,1,1}
+    }};
+}
+
+void draw_map() {
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            std::cout << map[i][j];
+        }
+        std::cout << '\n';
+    }
+}
--- main.h
+++ main.h
@@ -7,4 +7,5 @@ const int COL = 5;
 template <typename T, std::size_t Row, std::size_t Col>
 using Array2d = std::array<std::array<T, Col>, Row>;
 
-void draw_map(Array2d<int,ROW,COL> *map);
+void draw_map();
+void load_map();
