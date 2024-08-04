37642247b1eea8620df4a84fcdd990c81fb2d030
J F <cryonicfuse@gmail.com>
2024-08-04 10:08:21 -0700

    collision and movement

--- main.cpp
+++ main.cpp
@@ -5,35 +5,20 @@ Array2d<char,ROW,COL> memory;
 int pos_x, pos_y;
 
 int main() {
+    // Initialize curses library
     initscr();
     cbreak();
     noecho();
-
-    initialize_character(1,2);
+    // Initialize map and character placement
+    initialize_character(1,3);
     load_map();
-    update_memory();
-    draw_memory();
-    std::cout.flush();
-    int c = 0;
+    initialize_memory();
+    // Game Loop
     while (1) {
-        c = 0;
-        switch((c = getch())) {
-            case 65:
-                std::cout << "Up"; // key up
-                break;
-            case 66:
-                std::cout << "Down"; // key down
-                break;
-            case 67:
-                std::cout << "Right"; // key right
-                break;
-            case 68:
-                std::cout << "Left"; // key left
-                break;
-            default:
-                break;
-        }
-        std::cout.flush();
+        clear();
+        draw_memory();
+        handle_input();
+        
     }
     endwin();
     return 0;
@@ -41,11 +26,11 @@ int main() {
 
 void load_map() {
     map = {{
-                            {'1','1','1','1','1'},
-                            {'1','0','0','0','1'},
-                            {'1','0','1','0','1'},
-                            {'1','0','0','0','1'},
-                            {'1','1','1','1','1'}
+                            {'1','1','1','1','1'}, //0
+                            {'1','0','0','0','1'}, //1
+                            {'1','0','1','0','1'}, //2
+                            {'1','0','0','0','1'}, //3
+                            {'1','1','1','1','1'}  //4
     }};
 }
 
@@ -58,12 +43,51 @@ void draw_memory() {
     }
 }
 
-void update_memory() {
+void initialize_memory() {
     memory = map;
-    memory[pos_x][pos_y] = 'C';
+    memory[pos_y][pos_x] = 'C';
 }
 
 void initialize_character(int x, int y) {
     pos_x = x;
     pos_y = y;
+}
+
+void handle_input() {
+        int c = 0;
+        switch((c = getch())) {
+            case 65: // key up
+                if (valid_tile(pos_x, pos_y-1)) {
+                    move_character(pos_x, pos_y-1);
+                }
+                break;
+            case 66: // key down
+                 if (valid_tile(pos_x, pos_y+1)) {
+                    move_character(pos_x, pos_y+1);
+                }
+                break;
+            case 67: // key right
+                if (valid_tile(pos_x+1, pos_y)) {
+                    move_character(pos_x+1, pos_y);
+                }
+                break;
+            case 68: // key left
+                if (valid_tile(pos_x-1, pos_y)) {
+                    move_character(pos_x-1, pos_y);
+                }
+                break;
+            default:
+                break;
+        }
+}
+
+bool valid_tile(int x, int y) {
+    return memory[y][x] != '1';
+}
+
+void move_character(int x, int y) {
+    memory[pos_y][pos_x] = '0';
+    pos_x = x;
+    pos_y = y;
+    memory[pos_y][pos_x] = 'C';
 }
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #include <curses.h>
 #include <iostream>
 #include <array>
+#include<unistd.h> 
 
 const int ROW = 5;
 const int COL = 5;
@@ -10,5 +11,8 @@ using Array2d = std::array<std::array<T, Col>, Row>;
 
 void draw_memory();
 void load_map();
-void update_memory();
+void initialize_memory();
 void initialize_character(int,int);
+void handle_input();
+bool valid_tile(int,int);
+void move_character(int,int);
